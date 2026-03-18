f7f3a6b5ca3f02e1f9ae37e1601b062dcfcfa4ac
ToninhoDGP <266512803+ToninhoDGP@users.noreply.github.com>
2026-03-18 10:22:32 -0300

    Update calculadora_de_energia.py

--- calculadora_de_energia.py
+++ calculadora_de_energia.py
@@ -2,6 +2,9 @@ nome_aparelho = input("Digite o nome do aparelho: ")
 potencia = float(input(f"Digite a potência do(a) {nome_aparelho} em Watts (W): "))
 horas_dia = float(input(f"Quantas horas por dia o(a) {nome_aparelho} fica ligado(a)? "))
 consumo_mensal = (potencia * horas_dia * 30) / 1000
+valor_kwh = 0.75
+custo_estimado = consumo_mensal * valor_kwh
 print("-" * 30)
-print(f"Resultado para: {nome_aparelho}")
-print(f"O consumo mensal estimado é de: {consumo_mensal:.2f} kWh")
+print(f"Aparelho: {nome_aparelho}")
+print(f"Consumo estimado: {consumo_mensal:.2f} kWh/mês")
+print(f"Custo mensal estimado: R$ {custo_estimado:.2f}")
